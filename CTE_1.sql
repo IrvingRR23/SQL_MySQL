1c12ee9a7e759b6e355d5b586a36d93e4a8200ca
Irving Rodriguez <154105525+IrvingRR23@users.noreply.github.com>
2025-10-09 20:48:25 -0600

    Delete CTE_1.sql

--- CTE_1.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-Series.serie_id, 
-min(Episodios.fecha_estreno) As Min_fecha,
-Episodios.episodio_id
-FROM Series
-INNER JOIN Episodios
-ON Series.serie_id = Episodios.serie_id
-GROUP BY Episodios.episodio_id
